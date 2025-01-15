f8dbc3b79e56471876abdf568a26820e3bd7f232
Shaked Zychlinski <shakedzy@gmail.com>
2025-01-15 14:36:21 +0200

    improved logic

--- README.md
+++ README.md
@@ -2,22 +2,16 @@
 
 [![DOI](https://img.shields.io/badge/Join%20Telegram%20Group!--blue?logo=telegram&style=social)](https://t.me/models_watcher)
 
-This repo contains a script that scans Hugging Face models repository for new and modified base-models (meaning, models which are not derived fom other models),
-and send information summary to a dedicated Telegram group.
-
-Unlike the `huggingface_hub`'s `HfApi` object, which cannot tell apart new from modified models, this script analyzes the information
-found on Hugging Face's website (such as the model tree and files last-modification date) to extract additional information about each model, thus deciding if it's
-new or modified. See comments of `filter_base_models` and `analyze_model_tree` for more on this.
+This repo contains a script that scans Hugging Face models repository for new and modified base-models (models which are not derived fom other models) or models of leading organizations,
+and sends information summary to a dedicated Telegram group.
 
 ## Which models is it looking for?
 To try and narrow down the number of models, the watcher performs several filters:
-1. Time based - Only models which were created/modified in a given time-fram
+1. Time based - Only models which were created/modified in a given time-frame
 2. `model-index` - Only models with a valid `model-index` 
-3. Have dependents - Only models which are used as base models for other models (fine-tunes, quantizations, etc.)
-   - ׁIf no dependents were found (and the model is not a dependent of another), it is placed in the _undecided models_ list, and will be checked again according to the grace-time specified.
-
-The watcher only consideres models which match all the above criterias. 
-
+3. Have dependents _or_ from a leading organization - 
+   - Models which are used as base models for other models (fine-tunes, quantizations, etc.)
+   - ׁModels from leading organizations
 
 ## Running yourself
 If you'd like to run this script yourself, you'll first need a Bot Token and a Group ID.
@@ -30,5 +24,5 @@ pip install -r requirements.txt
 
 Then, you may run:
 ```bash
-python watcher.py [--days DAYS] [--hours HOURS] [--minutes MINUTES] [--grace-time GRACE]
+python watcher.py [--days DAYS] [--hours HOURS] [--minutes MINUTES]
 ```
--- watcher.py
+++ watcher.py
@@ -1,14 +1,13 @@
 import os
-import json
 import logging
 import asyncio
 import requests
-from tqdm import tqdm
 from telegram import Bot
 from bs4 import BeautifulSoup
-from huggingface_hub import HfApi
 from dataclasses import dataclass
 from argparse import ArgumentParser
+from collections import defaultdict
+from huggingface_hub import HfApi, ModelInfo
 from datetime import datetime, timedelta, timezone
 
 
@@ -19,42 +18,52 @@ GROUP_CHAT_ID = os.environ['GROUP_CHAT_ID']
 logger = logging.getLogger("models-watcher")
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
+hf_api = HfApi()
 
 
 @dataclass
 class ModelFile:
     filename: str
     is_directory: bool
-    change_time: datetime
 
 
 @dataclass
 class Model:
     model_id: str
     files: list[ModelFile]
-    is_new: bool
 
 
-def find_latest_models(time_threshold: datetime) -> list[str]:
-    api = HfApi()
-    models = api.list_models(sort="lastModified", direction=-1, full=True, filter="model-index")
-    recent_models = []
+def get_top_organizations(max_orgs: int = 100) -> list[str]:
+    models = hf_api.list_models(sort='downloads', direction=-1, limit=max_orgs * 5, full=True)
+    author_stats = defaultdict(lambda: {'model_count': 0, 'total_downloads': 0})
     for model in models:
-        if model.lastModified is None:
+        author = model.author
+        downloads = model.downloads if hasattr(model, 'downloads') and model.downloads else 0
+        author_stats[author]['model_count'] += 1
+        author_stats[author]['total_downloads'] += downloads
+    sorted_authors = sorted(author_stats.items(), key=lambda x: x[1]['total_downloads'], reverse=True)
+    top_authors: list[str] = [author for author, _ in sorted_authors]
+    
+    top_orgs: list[str] = []
+    for author in top_authors:
+        try: 
+            hf_api.get_user_overview(author, token=False)
             continue
-        last_modified = model.lastModified
-        if last_modified >= time_threshold:
-            recent_models.append(model.modelId)  # pyright: ignore[reportAttributeAccessIssue]
-        else:
-            break  
-    logger.info(f"Found {len(recent_models)} total models from relevant time-threshold on hugging-face")
-    return recent_models
+        except:
+            top_orgs.append(author)
+        finally:
+            if len(top_orgs) >= max_orgs:
+                break
+
+    logger.info(f"Retrieved {len(top_orgs)} top organizations from hugging-face:\n{top_orgs}")
+    return top_orgs
 
 
-def analyze_model_tree(model_id: str, time_threshold: datetime) -> Model | None:
+def find_modified_files(model_id: str, time_threshold: datetime) -> list[ModelFile]:
     response = requests.get(f"https://huggingface.co/{model_id}/tree/main")
     response.raise_for_status()
     soup = BeautifulSoup(response.content, "html.parser")
+    modified_files: list[ModelFile] = []
 
     # This section looks into the model repository page. 
     # The <ul> tag examined here is the table seen on the page, 
@@ -64,7 +73,7 @@ def analyze_model_tree(model_id: str, time_threshold: datetime) -> Model | None:
         # Only one <ul> has <time> tags in it, which are the last modified times of the files. 
         # This is the table we're looking for
         if ul.find_all("time"):
-            files: list[ModelFile] = []
+            
             for li in ul.find_all("li"):
                 # First <a> tag in <li> is the filename
                 filename = li.find("a").get_text(strip=True)  
@@ -73,59 +82,51 @@ def analyze_model_tree(model_id: str, time_threshold: datetime) -> Model | None:
                 is_directory = any(['text-blue' in c for c in li.find("svg").get("class")]) 
                 # The <time> tag is the last modified time of the file
                 change_time = datetime.strptime(li.find("time").get("datetime"), "%Y-%m-%dT%H:%M:%S").replace(tzinfo=timezone.utc)
-                files.append(ModelFile(filename=filename, is_directory=is_directory, change_time=change_time))
+                if change_time >= time_threshold:
+                    modified_files.append(ModelFile(filename=filename, is_directory=is_directory))
 
-            # A model is considered new if *all* files are newer than the time threshold
-            is_new = all([f.change_time >= time_threshold for f in files])
-            return Model(model_id=model_id, files=files, is_new=is_new)  
-    return None
+    return modified_files
 
 
-def filter_base_models(models: list[str], 
-                       undecided_models: list[str],
-                       time_threshold: datetime,
-                       grace_time_threshold: datetime
-                       ) -> tuple[list[Model], list[str]]:
-    base_models: list[Model] = []
-    new_undecided_models: list[str] = []
-    for model_id in tqdm(models + undecided_models, desc="Analyzing models"):
-        is_base = None
-        try:
-            response = requests.get(f"https://huggingface.co/{model_id}")
-            response.raise_for_status()
-            soup = BeautifulSoup(response.content, "html.parser")
-            for h2 in soup.find_all("h2"):
-                # Looking at the model tree shown on the model's page
-                # This section shows the relations between the current model and other known models
-                # If exists, it's always found on the <div> below a <h2> starting with the text "Model tree for"
-                if not "Model tree for" in h2.get_text(strip=True): continue   
-                div = h2.find_next_sibling("div")
-                if div:
-                    div_text = div.get_text(strip=True)
-                    # On Hugging Face model page, if the model tree displays a model
-                    # with the text "Base model", it's always *another* model, meaning 
-                    # the current model is a derived model and not a base model.
-                    # Base model always has a tree, but without the text "Base model" in it 
-                    # (it has the model's name in it).
-                    is_base = "Base model" not in div_text
-                if is_base:
-                    model = analyze_model_tree(model_id, time_threshold=grace_time_threshold if model_id in undecided_models else time_threshold) 
-                    if model:
-                        base_models.append(model)
-                break
+def keep_single_model(model: ModelInfo) -> bool:
+    card_data = model.card_data
+    if card_data:
+        base_model = card_data.base_model
+        return not base_model
+    else:
+        return False
 
-        except Exception as e:
-            logger.error(f"Failed to analyze model {model_id} - {e.__class__}: {e}") 
 
-        finally:
-            if is_base is None:
-                # In the case where the <div> of the model tree is not found,
-                # the model is placed in the undecided models list, and will be checked again later.
-                # This is to avoid false negatives, as the model might be a base model but the tree is not loaded yet.
-                new_undecided_models.append(model_id)
+def find_new_models(time_threshold: datetime, top_orgs: list[str]) -> list[Model]:
+    models = hf_api.list_models(sort="created_at", direction=-1, full=True, filter="model-index")
+    recent_models: list[str] = []
+    for model in models:
+        if model.created_at is None:
+            continue
+        elif model.created_at >= time_threshold:
+            if model.author in top_orgs or keep_single_model(model):
+                recent_models.append(model.id)
+        else:
+            break  
+    logger.info(f"Found {len(recent_models)} new models on hugging-face")
+    return [Model(m, []) for m in recent_models]
 
-    logger.info(f"Found {len(base_models)} base models from models list, {len(new_undecided_models)} undecided models")
-    return base_models, new_undecided_models     
+
+def find_modified_models(time_threshold: datetime, top_orgs: list[str]) -> list[Model]:
+    models = hf_api.list_models(sort="last_modified", direction=-1, full=True, filter="model-index")
+    recent_models: list[Model] = []  
+    for model in models:
+        if model.last_modified is None or model.created_at is None:
+            continue
+        elif model.last_modified >= time_threshold:
+            if model.created_at < time_threshold:
+                if model.author in top_orgs or keep_single_model(model):
+                    files = find_modified_files(model.id, time_threshold)
+                    recent_models.append(Model(model_id=model.id, files=files))
+        else:
+            break  
+    logger.info(f"Found {len(recent_models)} modified models on hugging-face")
+    return recent_models
 
 
 def escape_markdown(text: str, 
@@ -137,12 +138,12 @@ def escape_markdown(text: str,
     return text
 
 
-def prepare_message(*, undecided_model_ids: list[str], time_threshold: datetime, grace_time_threshold: datetime) -> tuple[str, list[str]]:
-    model_ids = find_latest_models(time_threshold)
-    models, new_undecided_models = filter_base_models(model_ids,undecided_model_ids, time_threshold, grace_time_threshold)
+def prepare_message(time_threshold: datetime) -> str:
+    top_orgs = get_top_organizations()
+    new_models = find_new_models(time_threshold, top_orgs)
+    modified_models = find_modified_models(time_threshold, top_orgs)
+
     message = ""
-    new_models = [m for m in models if m.is_new]
-    modified_models = [m for m in models if not m.is_new]
     if new_models:
         message += f"🆕 *New models:*\n"
         for model in new_models:
@@ -150,35 +151,10 @@ def prepare_message(*, undecided_model_ids: list[str], time_threshold: datetime,
     if modified_models:
         message += f"\n🔄 *Modified models:*\n"
         for model in modified_models:
-            modified_files = [f"{'Content of ' if f.is_directory else ''}{f.filename}{'/' if f.is_directory else ''}" for f in model.files if f.change_time >= time_threshold]
+            modified_files = [f"{'Content of ' if f.is_directory else ''}{f.filename}{'/' if f.is_directory else ''}" for f in model.files]
             modified_files = [escape_markdown(f) for f in modified_files]
             message += f" • [{escape_markdown(model.model_id)}](https://huggingface.co/{escape_markdown(model.model_id, chars=['('])}) _\\(Updated files: {', '.join(modified_files)}\\)_\n"
-    return message.strip(), new_undecided_models
-
-
-def load_undecided_models(grace_time_threshold: datetime) -> dict[str, datetime]:
-    loaded_models = {}
-    try:
-        with open("undecided_models.json", "r") as f:
-            models = {k: datetime.strptime(v, "%Y-%m-%dT%H:%M:%S").replace(tzinfo=timezone.utc) for k, v in json.load(f).items()}
-            models = {k: v for k,v in models.items() if v >= grace_time_threshold}
-            loaded_models = models
-    except Exception as e:
-        logger.error(f"Failed to load undecided models - {e.__class__}: {e}")
-    finally:
-        logger.info(f"Loaded {len(loaded_models)} undecided models:\n{loaded_models}")
-        return loaded_models
-    
-
-def save_undecided_models(existing_undecided_models: dict[str, datetime], new_undecided_models: dict[str, datetime]) -> None:
-    undecided_models = new_undecided_models | existing_undecided_models
-    undecided_models = {k: datetime.strftime(v, "%Y-%m-%dT%H:%M:%S") for k,v in undecided_models.items()}
-    try:
-        with open("undecided_models.json", "w") as f:
-            json.dump(undecided_models, f)
-        logger.info(f"Saved {len(undecided_models)} undecided models:\n{undecided_models}")
-    except Exception as e:
-        logger.error(f"Failed to save {len(undecided_models)} undecided models - {e.__class__}: {e}")
+    return message.strip()
 
 
 async def send_group_message(message: str):
@@ -191,20 +167,13 @@ def main():
     parser.add_argument("--days", type=int, default=0, help="Number of days to look back")
     parser.add_argument("--hours", type=int, default=0, help="Number of hours to look back")
     parser.add_argument("--minutes", type=int, default=0, help="Number of minutes to look back")
-    parser.add_argument("--grace-time", dest='grace', type=int, default=5, help="Number of hours of grace time for undecided models")
     args = parser.parse_args()
 
     assert any([args.days, args.hours, args.minutes]), "At least one of the time arguments (days/hours/minutes) must be greater than 0"
     delta = timedelta(days=args.days, hours=args.hours, minutes=args.minutes)
     time_threshold = datetime.now(timezone.utc) - delta
-    grace_delta = timedelta(hours=args.grace)
-    grace_time_threshold = time_threshold - grace_delta
 
-    undecided_models = load_undecided_models(grace_time_threshold)
-    logger.info(f"Loaded {len(undecided_models)} undecided models")
-    message, new_undecided_models_list = prepare_message(undecided_model_ids=list(undecided_models.keys()), time_threshold=time_threshold, grace_time_threshold=grace_time_threshold)
-    save_undecided_models(undecided_models, {k: datetime.now(timezone.utc) for k in new_undecided_models_list})
-
+    message = prepare_message(time_threshold)
     if message:
         logger.info(f"Sending message:\n{message}")
         asyncio.run(send_group_message(message))
